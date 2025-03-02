23c1147e3d9d0338bab93b82e94c7fd1fb215908
Muath Alwadeai <moaath.wadei@gmail.com>
2025-03-03 01:52:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ backgroundColor: "#19043d", width: "100vw", height: "100vh" }}
+      className="App"
+    >
+      <div
+        style={{
+          backgroundColor: "#4b10b0",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          padding: "20px",
+        }}
+      >
+        <h3 style={{ color: "white" }}>Requesting a Loan</h3>
+        <hr></hr>
+
+        <label>Name</label>
+        <input></input>
+        <label>Phone Number</label>
+        <input></input>
+        <label>Age</label>
+        <input></input>
+        <label>Are you an employee?</label>
+        <input></input>
+        <label>Salary</label>
+        <input></input>
+        <button type="submit">Submit</button>
+      </div>
     </div>
   );
 }
